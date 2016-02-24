08b90a6d8d37e38aebfc28703608d62bebaee25b
Michael Wilder <mwilder.w@gmail.com>
2016-02-24 14:36:57 -0500

    Setup X3D

--- src/main.c
+++ src/main.c
@@ -1,3 +1,25 @@
-int main() {
+#include "X3D.h"
+
+#include <SDL/SDL.h>
+
+void init() {
+  X3D_InitSettings init = {
+    .screen_w = 320,
+    .screen_h = 240,
+    .screen_scale = 1,
+    .fullscreen = X3D_FALSE,
+    .fov = ANG_60
+  };
+  
+  x3d_init(&init);
+}
+
+
+int main(int argc, char *argv[]) {
+  init();
+  
+  SDL_Delay(5000);
+  
+  x3d_cleanup();
   
 }
